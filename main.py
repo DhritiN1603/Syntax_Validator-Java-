e89aebfc1dcf598cf9f628e1a26347cabda1f90d
DhritiN1603 <82032988+DhritiN1603@users.noreply.github.com>
2024-02-28 20:55:31 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from lexer import lexer
+from parser1 import parser
+
+ # we know the file exists , so goes into try block , once error encontered goes to except block 
+
+try:
+    with open("D:\Downloads\Another Example\project\new\input1.txt", "r") as file:
+        data = file.read()
+        print()
+        print("The Input File Content:\n")
+        print(data)
+        print()
+        parser.parse(data, lexer=lexer)
+        print("Parsing has been completed!\n")
+
+except Exception as e:
+    print('Syntax Error!!')
+    print(f"Error during parsing: {e}")
